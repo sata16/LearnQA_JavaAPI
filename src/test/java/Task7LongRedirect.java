5166d7c7cdb517afbe5757fffa30b04909241480
slyusar <tslusar@yandex.ru>
2023-04-01 23:29:48 +0300

    Task6

---
+++ src/test/java/Task7LongRedirect.java
@@ -0,0 +1,28 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Task7LongRedirect {
+
+    @Test
+    public void testLongRedirect(){
+        String urlTest = "https://playground.learnqa.ru/api/long_redirect";
+       do {
+           Response response = RestAssured
+                   .given()
+                   .redirects()
+                   .follow(false)
+                   .when()
+                   .get(urlTest)
+                   .andReturn();
+           int statusCode = response.getStatusCode();
+           System.out.println(statusCode);
+           urlTest = response.getHeader("Location");
+           System.out.println(urlTest);
+       } while (urlTest != null);
+
+
+    }
+
+
+}
