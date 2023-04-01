f864f12a52c4ef9de9a115be5a3b3768681fc7b5
slyusar <tslusar@yandex.ru>
2023-04-01 12:58:20 +0300

    helloworldtestAPI

---
+++ src/test/java/HelloWorldTestAPI.java
@@ -0,0 +1,13 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldTestAPI {
+    @Test
+    public void HelloWorldTestAPI(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
