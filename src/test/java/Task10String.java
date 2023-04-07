83b83836635f47428fafaf3fdb28e392de672a65
slyusar <tslusar@yandex.ru>
2023-04-07 21:45:09 +0300

    Task10

---
+++ src/test/java/Task10String.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Task10String {
+
+    @Test
+    public void testString(){
+        String text1 = "fhyhjdnfhydbnhy";
+        assertTrue(text1.length()>15,"invalid lenght "+ text1.length());
+
+    }
+
+
+}
