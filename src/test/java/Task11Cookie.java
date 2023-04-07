1ffd3851456331b211b6cc30da02f84731901aec
slyusar <tslusar@yandex.ru>
2023-04-07 22:04:56 +0300

    Task10

---
+++ src/test/java/Task11Cookie.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Task11Cookie {
+    @Test
+    public void testCookie(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+
+//        Map<String,String> responseCookie = response.getCookies();
+//        System.out.println(responseCookie);
+        String responseCookies = response.getCookie("HomeWork");
+        assertEquals("hw_value",responseCookies, "invalid cookies");
+
+    }
+
+
+
+
+}
